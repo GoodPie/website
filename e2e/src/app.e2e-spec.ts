fff269457d47f488a7a7bc743336d06fe6897cc5
Brandyn <Brandynbb96@gmail.com>
2019-11-29 19:35:03 +0800

    Added Authentication
    - Authentication ready for project implementation

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('personalwebsite app is running!');
+    expect(page.getTitleText()).toEqual('personalwebsite assets is running!');
   });
 
   afterEach(async () => {
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,7 @@
+export interface User {
+  uid: string;
+  email: string;
+  profilePicture?: string;
+  displayName?: string;
+  level?: number;
+}
