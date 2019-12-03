1c3e64c7192c51c004cedae1b83bdcfb9459e364
Brandyn <Brandynbb96@gmail.com>
2019-12-03 16:44:59 +0800

    Fixed route animation ready for deployment

--- src/app/route.animation.ts
+++ src/app/route.animation.ts
@@ -7,37 +7,91 @@ import {
   animate,
 } from '@angular/animations';
 
-
 export const slider =
   trigger('routeAnimations', [
-    transition('* => isLeft' , slideTo('right')),
-    transition('* => isRight', slideTo('left')),
-    transition('isRight => *', slideTo('left')),
-    transition('isLeft => *', slideTo('right'))
-  ]);
-
-
-function slideTo(direction) {
-  const optional = {optional: true}
-  return [
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: '65px',
-        [direction]: 0,
-        width: '100%'
-      })
-    ], optional),
-    query(':enter', [
-      style({[direction] : '-100%'})
-    ]),
-    group([
-      query(':leave', [
-        animate('500ms ease', style({[direction]: '100%'}))
-      ], optional),
-      query(':enter', [
-        animate('500ms ease', style({[direction]: '0%'}))
+      transition('* => isLeft', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: '64px',
+            left: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ left: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('500ms ease', style({ left: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('500ms ease', style({ left: '0%'}))
+          ])
+        ]),
+      ]),
+      transition('* => isRight', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: '64px',
+            right: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ right: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('500ms ease', style({ right: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('500ms ease', style({ right: '0%'}))
+          ])
+        ]),
+      ]),
+      transition('isRight => *', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: '64px',
+            right: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ left: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('500ms ease', style({ left: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('500ms ease', style({ left: '0%'}))
+          ])
+        ]),
+      ]),
+      transition('isLeft => *', [
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: '64px',
+            right: 0,
+            width: '100%'
+          })
+        ], { optional: true }),
+        query(':enter', [
+          style({ right: '-100%'})
+        ]),
+        group([
+          query(':leave', [
+            animate('500ms ease', style({ right: '100%'}))
+          ], { optional: true }),
+          query(':enter', [
+            animate('500ms ease', style({ right: '0%'}))
+          ])
+        ]),
       ])
-    ])
-  ];
-}
+    ],
+  );
